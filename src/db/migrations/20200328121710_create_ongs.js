0c9521e817c09b4ec0ce8b1d5bf9a70bcc49b8de
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-03-28 15:59:29 -0300

    Add migration files to create the db tables

---
+++ src/db/migrations/20200328121710_create_ongs.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('ongs', function(table) {
+      table.string('id').primary();
+
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('phone').notNullable();
+      table.string('city').notNullable();
+      table.string('state', 2).notNullable();
+
+      table.timestamps();
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('ongs');
+};
